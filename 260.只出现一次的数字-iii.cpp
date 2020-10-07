ef6799c0468ca9f7e81d5d2fdc99ea0c9681616b
zhengshiwei <3020918705@qq.com>
2020-10-07 20:21:42 +0800

    260题提交

---
+++ 260.只出现一次的数字-iii.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=260 lang=cpp
+ *
+ * [260] 只出现一次的数字 III
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int mask=0;
+        for(int i:nums)
+        {
+            mask^=i;
+        }
+        int diff=mask&(-mask);
+        vector<int> v;
+        int res1=0;
+        for(int i:nums)
+        {
+            if(i&diff)
+            {
+                res1^=i;
+            }
+        }
+        v.push_back(res1);
+        v.push_back(res1^mask);
+        return v;
+    }
+};
+// @lc code=end
+
